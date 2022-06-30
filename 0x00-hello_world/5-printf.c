fd033ececa94ca6546b947ba8d2dea9c01b9af6c
derrickdelali <derrickdelali@outlook.com>
2022-06-30 15:41:45 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -11,5 +11,5 @@
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
