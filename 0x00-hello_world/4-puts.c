42504436b6e52d98cc233c9fac4072b07b8bc383
derrickdelali <derrickdelali@outlook.com>
2022-06-30 15:24:52 -0700

    First C Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry
+ *
+ * Description: A C program that prints with a put function
+ *
+ * Return: Always return with a zero value
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
