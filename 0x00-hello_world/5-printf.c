6a10dbb0c6ead7099af45ef0fc02683f245409d5
derrickdelali <derrickdelali@outlook.com>
2022-06-30 15:30:09 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main: Entry
+ *
+ * Description: A C Program that prints with a printf function.
+ *
+ * Return: Always return with a zero value
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
