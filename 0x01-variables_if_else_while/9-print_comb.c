2135977b3a653727d7de68c322f0354cf82f8dba
derrickdelali <derrickdelali@outlook.com>
2022-07-01 04:06:57 -0700

    single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry
+ *
+ * description: print all possible combinations of single digit numbers
+ *
+ * return: zero always
+ *
+ */
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i == '9')
+		{
+			putchar('\n');
+			return (0);
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+			i++;
+		}
+	}
+	return (0);
+}
