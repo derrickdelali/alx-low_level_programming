bd999b84f8dfd6328e0815609af0e9fbf44e2531
derrickdelali <derrickdelali@outlook.com>
2022-07-01 02:39:16 -0700

    0_+_-

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
 		printf("%i is positive\n", n);
-	}else if ( n == 0)
-	{
+	else if ( n == 0)
 		printf("%i is zero\n", n);
-	}else
-	{
+	else
 		printf("%i is negative\n", n);
-	}
+	
 
 	return (0);
 }
