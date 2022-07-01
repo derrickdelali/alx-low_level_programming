18d881fc53794813904a86fe2de48d9c72ef7c4b
derrickdelali <derrickdelali@outlook.com>
2022-06-30 17:17:30 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-printf(stderr, "with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
