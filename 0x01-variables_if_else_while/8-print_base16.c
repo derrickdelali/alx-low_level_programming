f4b62ea21d7c45262d7a71df2e2ce771266b3745
derrickdelali <derrickdelali@outlook.com>
2022-07-01 03:58:37 -0700

    alphanumeric base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry
+ *
+ * description: print all numbers in base 16
+ *
+ * retun: Always zero
+ *
+ */
+
+int main(void)
+{
+	char i = '0';
+	char j = 'a';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
