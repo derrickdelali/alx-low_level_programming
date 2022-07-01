14df50d0d7003a940dce4b224491a28db75bd544
derrickdelali <derrickdelali@outlook.com>
2022-07-01 03:47:53 -0700

    Reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * description: print lowecase alphabets in reverse
+ *
+ * return: Zero always
+ *
+ */
+
+int main(void)
+{
+	char i = 'z';
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
