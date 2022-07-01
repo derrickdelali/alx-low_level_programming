2817070c6aa32e59f8c46c79a548b928a6372130
derrickdelali <derrickdelali@outlook.com>
2022-07-01 03:38:16 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry
+ *
+ * description: print numbers of base 10 starting from 0
+ *
+ * return: always zero
+ *
+ */
+
+int main(void)
+{
+	int i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
