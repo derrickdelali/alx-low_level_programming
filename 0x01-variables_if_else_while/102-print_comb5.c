62362f2250a83ef451b2fe16e517e542ad73bfe5
derrickdelali <derrickdelali@outlook.com>
2022-07-01 04:59:27 -0700

    print comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry
+ *
+ * description: print two two digit numbers combinations
+ *
+ * return: zero always
+ */
+
+int main(void)
+{
+	int ones = 0,
+	    int tens;
+
+	while (ones <= 99)
+	{
+		tens = ones;
+		while (tens <= 99)
+		{
+			if (tens != ones)
+			{
+				putchar((ones / 10) + 48);
+				putchar((ones % 10) + 48);
+				putchar(' ');
+				putchar((tens / 10) + 48);
+				putchar((tens % 10) + 48);
+
+				if (ones != 98 || tens != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++tens;
+		}
+		++ones;
+	}
+	putchar('\n');
+	return (0);
+}
