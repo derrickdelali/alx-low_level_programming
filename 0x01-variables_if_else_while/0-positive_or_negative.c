79cebf3eb9248a3daa02a7e880e7d10e7ee3657a
derrickdelali <derrickdelali@outlook.com>
2022-07-01 13:37:49 -0700

    Positive Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,11 +19,10 @@ int main(void)
 
 	if (n > 0)
 		printf("%i is positive\n", n);
-	else if ( n == 0)
+	else if (n == 0)
 		printf("%i is zero\n", n);
 	else
 		printf("%i is negative\n", n);
-	
 
 	return (0);
 }
