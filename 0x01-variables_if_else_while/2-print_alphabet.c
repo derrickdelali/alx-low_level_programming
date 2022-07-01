6d4c6cc439ff31f750f90888cf43f85a8a7fe2a8
derrickdelali <derrickdelali@outlook.com>
2022-07-01 03:03:47 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * description: print alphabets in a lowercase
+ *
+ * retun: 0
+ *
+ */
+
+int main(void)
+{
+	char i = 'i';
+
+	for (i = 'i'; i <= 'j'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
