5a7d5356dc2ffd1a20699f1d204e9cf3c2359c1a
derrickdelali <derrickdelali@outlook.com>
2022-07-06 16:08:44 -0700

    j

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_last_digit - checks number
+ *
+ * @j: Takes input
+ *
+ * Return: 
+ */
+
+int print_last_digit(int j)
+{
+	int i;
+
+	int i = j % 10;
+	
+	if (i < 0)
+	{
+		i*= -1;
+	}
+	_putchar(i + '0');
+	return (i);
