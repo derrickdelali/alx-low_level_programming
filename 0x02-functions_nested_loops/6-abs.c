f32dc64bf068646db1b6bfb6a8876c58b1b711f5
derrickdelali <derrickdelali@outlook.com>
2022-07-06 16:02:13 -0700

    r

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * _abs - Checks numbers
+ *
+ * @r: takes input
+ *
+ * Return: r if its true
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
