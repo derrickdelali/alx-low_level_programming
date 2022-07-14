1a1be4753fc3db2a1c1b8ad354e58bc667187c3f
derrickdelali <derrickdelali@outlook.com>
2022-07-14 14:43:29 -0700

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * puts2 - main function.
- * @str: Pointer, the string to print.
+ *puts2 - prints every character of a string
+ *@str: A pointer to an int that will be changed
  *
- * Description: This function prints every other character of a string
- * (divisible by two)
- * Return: none.
+ *Return: void which means it is correct
  */
 
 void puts2(char *str)
 {
-	int i;
+int a;
 
-	while (str[i])
-	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar (str[a]);
+}
 
-		i++;
-	}
-	_putchar('\n');
+_putchar ('\n');
 }
